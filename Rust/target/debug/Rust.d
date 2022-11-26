7f2758134a0afc91f7f18fe94fca8b0e5ad88eb8
Anh H Nguyen <aaanh@users.noreply.github.com>
2022-11-26 17:58:57 -0500

    reorg and init rust

---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+C:\Users\anh\repos\meaningful-graph-traversal\Rust\target\debug\Rust.exe: C:\Users\anh\repos\meaningful-graph-traversal\Rust\src\main.rs
---
+++ Rust/target/debug/deps/Rust.d
@@ -0,0 +1,5 @@
+C:\Users\anh\repos\meaningful-graph-traversal\Rust\target\debug\deps\Rust.exe: src\main.rs
+
+C:\Users\anh\repos\meaningful-graph-traversal\Rust\target\debug\deps\Rust.d: src\main.rs
+
+src\main.rs:
